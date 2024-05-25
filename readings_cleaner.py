050c5cfaa5ad31fef8d1162b884be2b83e63f5b2
borazslo <eleklaszlosj@gmail.com>
2024-05-26 00:07:09 +0200

    szöveg elejéről és végéről sortörések törlése

---
+++ readings_cleaner.py
@@ -0,0 +1,50 @@
+import simplejson
+
+def loadKatolikusData():
+    katolikusData = {}
+    sources = ["olvasmanyok", "vasA", "vasB", "vasC","szentek","commentaries"]
+    for name in sources:
+        with open('readings/' + name + '.json', 'r',encoding="utf8") as file:
+            katolikusData[name] = simplejson.load(file)
+    return katolikusData
+
+def saveKatolikusData(katolikusData):
+    for key, name in enumerate(katolikusData):
+        with open("readings/" + name + ".json", "w", encoding='utf8') as katolikusDataFile:        
+            katolikusDataFile.write(
+                simplejson.dumps(katolikusData[name], indent=4, sort_keys=False, ensure_ascii=False)
+            )
+
+def recursive(data, function):    
+    if isinstance(data, str):
+        return eval(function + '(data)')
+    elif isinstance(data, list):
+        returnList = []
+        for item in data:
+            returnList.append(recursive(item, function))
+        return returnList
+    elif isinstance(data, dict):
+        returnDict = {}
+        for key, name in enumerate(data):
+            returnDict[name] = recursive(data[name], function)
+        return returnDict
+    elif data is None:
+        return None
+        
+    raise Exception("Sorry, do not know this type")
+
+def remove_rlnewline(data):
+    return data.rstrip('\n').lstrip('\n').removeprefix("<br>").removesuffix("<br>")
+
+
+
+   
+katolikusData = loadKatolikusData()
+
+
+
+
+katolikusData = recursive(katolikusData, "remove_rlnewline")
+print(katolikusData)
+
+saveKatolikusData(katolikusData)
