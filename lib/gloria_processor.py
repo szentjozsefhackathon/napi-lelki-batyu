ed7c41c6318b21a4f602d3c13d432abd97ff8a31
woksihd <woksihd@gmail.com>
2026-04-18 15:44:23 +0200

    Gloria

--- lib/gloria_processor.py
+++ lib/gloria_processor.py
@@ -7,21 +7,22 @@ from lib import (
 )
 
 gloriaTypes = {
-    "van": "Hitvallás van",
+    "van": "Dicsőség",
     "nincs": "",
-    "keneDeNincs": "Nincs hitvallás"
+    "keneDeNincs": "Dicsőség nincs"
 }
 
 def main (celebration: Dict[str, Any]) -> str:
     try:
         dayOfWeek = celebration["dayofWeek"]
         season = celebration["season"]
-        
+        level = celebration["level"]
+        celebrationType = celebration["celebrationType"]
     except Exception as e:
         error_handler.error(str(e))
         return "default"
     try:
-        if (dayOfWeek == 0):
+        if (dayOfWeek == 0 and season != 6):
             gloria = "van"
         else:
             gloria = "nincs"
