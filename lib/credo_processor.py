a58d0450fd1e8827189901caecc40c4deccdad69
woksihd <woksihd@gmail.com>
2026-04-18 20:53:27 +0200

    fix logics and extend térdhajtás

--- lib/credo_processor.py
+++ lib/credo_processor.py
@@ -9,6 +9,7 @@ from lib import (
 credotypes = {
     "van": "Hitvallás van",
     "nincs": "",
+    "terdhajtas": "Hitvallás (térdhajtással)",
     "keneDeNincs": "(nincs Hitvallás)",
     "szekesegyhaz": "(a székesegyházban van, azon kívül nincs Hitvallás)",
     "szent": "(az egyházmegyében van, azon kívül nincs Hitvallás)"
@@ -21,37 +22,52 @@ def main (celebration: Dict[str, Any]) -> str:
         level = celebration["level"]
         name = celebration["name"]
         typeLocal = celebration["typeLocal"]
+        dateISO = celebration["dateISO"]
     except Exception as e:
         error_handler.error(str(e))
         return "default"
     try:
+        ##### Amikor biztosan van
         if (
-                (dayOfWeek == "0") or
-                (level == "6") or
+                ##### Karácsony és Évközi minden vasárnap
+                (dayOfWeek == 0) or (level == "6") or
+                #### Minden 3-as szintű kivéve Halottak napja
                 (level == "3" and name != "Halottak napja") or
+                #### Minden 2-es szintű kivéve ha ....
+                #### Hamavazószerda, Húsvét nyolcad, nagyböjti időszak és karácsony nyolcada
+                #### DE nagycsütörtök Krizmaszentelőjén sem
                 (level == "2" and
                     (
                         name != "Hamvazószerda" and
                         not "krizma" in str(name).lower() and
-                        season != "7"
+                        season != "7" and
+                        season != "9" and
+                        season != "2"
                     )
                 )
             ):
             credo = "van"
         else:
+            #### Karácsony 12-25 minden miséjén térdhajtással
             if (
-                season == "9" or
-                season == "3"
-            ):
-                credo = "keneDeNincs"
+                    (season == "2" and "12-25" in str(dateISO).lower())
+                ):
+                    credo = "terdhajtas"
             else:
-                if "székes" in str(typeLocal).lower():
-                    credo = "szekesegyhaz"
+                ##### Húsvét és karácsony nyolcadában kéne DE nincs!
+                if (
+                    season == "9" or
+                    (season == "2" and not "12-25" in str(dateISO).lower())
+                ):
+                    credo = "keneDeNincs"
                 else:
-                    if "védő" in str(typeLocal).lower():
-                        credo = "szent"
+                    if "székes" in str(typeLocal).lower():
+                        credo = "szekesegyhaz"
                     else:
-                        credo = "nincs"
+                        if "védő" in str(typeLocal).lower():
+                            credo = "szent"
+                        else:
+                            credo = "nincs"
     except Exception as e:
         error_handler.error(str(e))
         return "default"
